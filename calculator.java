45dfa629d6b033262070fe788e892a1d6b8e103d
shivuH210 <mahendrasaichitta@gmail.com>
2024-02-08 11:32:48 +0530

    Code Related to Basic Airthmatic ops

--- calculator.java
+++ calculator.java
@@ -2,14 +2,29 @@ public class calculator{
    float addition(float operand_1,float operand_2){
     return(operand_1+operand_2);
    }
+ /**
+  * The function "substraction" takes two float operands and returns their difference.
+  * 
+  * @param operand_1 The first operand for the subtraction operation.
+  * @param operand_2 The second operand in the subtraction operation.
+  * @return the result of subtracting operand_2 from operand_1.
+  */
    float substraction(float operand_1,float operand_2){
     return(operand_1 - operand_2);
    }
+   // The code you provided defines two methods in the `calculator` class: `multiplication` and
+   // `division`.
    float multiplication(float operand_1,float operand_2){
     return(operand_1 * operand_2);
    }
    float division(float operand_1,float operand_2){
     return(operand_1 / operand_2);
+   }
+   float square (float operand_1){
+    return (operand_1*operand_1);
+   }
+   float cube(float operand_1){
+    return(operand_1*operand_1*operand_1);
    }
 
    public static void main(String[] args) {
@@ -24,6 +39,10 @@ public class calculator{
     System.out.println("Multiplication Result is: " +multi_result);
     float division_result =calc.division(operand_1, operand_2);
     System.out.println("division Result is: " +division_result);
+    float square_result =calc.square(operand_1);
+    System.out.println("square Result is: " +square_result);
+    float cube_result =calc.cube(operand_1);
+    System.out.println("cube Result is: " +cube_result);
 
    }
 
